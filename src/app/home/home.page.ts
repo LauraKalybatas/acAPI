34f25b193b0365e7ea38f03b7cdd6023ff9024d3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-19 08:03:11 -0300

    ac

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cep: string = '';
+  estado: string = '';
+  cidade: string = '';
+  logradouro: string = '';
+  resultado$: Observable<any> | undefined; 
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+
+  consultarCep() {
+    if (this.cep) {
+      this.resultado$ = this.http.get(`https://viacep.com.br/ws/${this.cep}/json`);
+    }
+  }
+
+
+  consultarEndereco() {
+    if (this.estado && this.cidade && this.logradouro) {
+      this.resultado$ = this.http.get(`https://viacep.com.br/ws/${this.estado}/${this.cidade}/${this.logradouro}/json`);
+    }
+  }
 }
